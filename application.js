5485c1c4b8170b4c8be34e27ecce62a1f1358859
Bruno Henrique Stein <brunohstein@gmail.com>
2013-02-10 21:02:10 -0200

    only styles left

--- application.js
+++ application.js
@@ -19,13 +19,47 @@ var Item = function(name, restriction_level, rarity, img, max_offer_unit_price, 
 
 Item.prototype.getName          = function() { return this.name; };
 Item.prototype.getLevel         = function() { return this.level; };
-Item.prototype.getrarity        = function() { return this.rarity; };
+Item.prototype.getRarity        = function() {
+  if (this.rarity == 0) { return 'junk' }
+  else if (this.rarity == 1) { return 'common' }
+  else if (this.rarity == 2) { return 'fine' }
+  else if (this.rarity == 3) { return 'masterwork' }
+  else if (this.rarity == 4) { return 'rare' }
+  else if (this.rarity == 5) { return 'exotic' }
+  else if (this.rarity == 6) { return 'legendary' };
+};
 Item.prototype.getImage         = function() { return this.image; };
-Item.prototype.getMaxOfferPrice = function() { return this.max_offer_price; };
+Item.prototype.getMaxOfferPrice = function() { return toGSC(this.max_offer_price); };
 Item.prototype.getOfferQuantity = function() { return this.offer_quantity; };
-Item.prototype.getMinSalePrice  = function() { return this.min_sale_price; };
+Item.prototype.getMinSalePrice  = function() { return toGSC(this.min_sale_price); };
 Item.prototype.getSaleQuantity  = function() { return this.sale_quantity; };
-Item.prototype.printItem        = function() { $('ul', '#results').append('<li>' + this.getName() + '</li>'); };
+Item.prototype.printItem = function() {
+  $('ul', '#results').append(
+    ['<li>',
+      '<img src="' + this.getImage() + '" />',
+      '<span class="name ' + this.getRarity() + '">'+ this.getName() + '</span>',
+      '<span class="level">'+ this.getLevel() + '</span>',
+      '<span class="offer">'+ this.getMaxOfferPrice() + '</span>',
+      '<span class="sale">'+ this.getMinSalePrice() + '</span>',
+    '</li>'].join('')
+  );
+};
+
+toGSC = function(val) {
+  var openG = '<span class="gold">';    var closeG = '</span>';
+  var openS = '<span class="silver">';  var closeS = '</span>';
+  var openC = '<span class="copper">';  var closeC = '</span>';
+
+  var sign = (val < 0 ? -1 : 1);
+  var g = Math.floor(sign * val / 10000);
+  var s = Math.floor((sign * val - g * 10000) / 100);
+  var c = sign * val - g * 10000 - s * 100;
+  
+  if (g) return openG + (sign * g) + closeG + openS + s + closeS + openC + c + closeC;
+  if (s) return openS + (sign * s) + closeS + openC + c + closeC;
+  if (c) return openC + (sign * c) + closeC;
+  return openC + '0' + closeC;
+};
 
 $(document).ready(function() {
 
